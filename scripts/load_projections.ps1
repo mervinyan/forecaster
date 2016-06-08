44def84905538aae10054972535968c4d93cbf6a
Mervin Yan <myan@paradigm.sk.ca>
2016-06-08 06:01:13 -0600

    initial commit s for cashflow forecaster app

---
+++ scripts/load_projections.ps1
@@ -0,0 +1,8 @@
+$projections = 'projection_account_info',  'projection_cashflow'
+Foreach ($projection in $projections)
+{
+	$fileName = '@C:\my_workspaces\forecaster\app\projections\' + $projection + '.js'
+	$streamId = 'http://127.0.0.1:2113/projections/continuous?emit=yes&checkpoints=yes&enabled=yes&name=' + $projection
+	$args = @('-i', '-X', 'POST', '-d', $fileName, $streamId, '-H', 'Content-Type:application/json', '-u', 'admin:changeit')
+	& 'C:\my_workspaces\curl.exe' $args
+}
