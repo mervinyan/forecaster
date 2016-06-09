5fdc033eb5e7b6a93bb5cf2aff7d9cec4bcaa952
mervinyan <mervin.yan@gmail.com>
2016-06-09 16:57:51 -0600

    updated balances projection

--- app/projections/projection_balances.js
+++ app/projections/projection_balances.js
@@ -1,11 +1,11 @@
-fromStream('transactionstream-6ee1da56-7648-40d6-95bb-fd16eeb40503')
+fromCategory('account')
     .when({
         $init: function () {
             return {
                 balances: {}
             }
         },
-        "TransactionOccurred": function (state, ev) {
+        "ActualImported": function (state, ev) {
             if (!state.balances[ev.body["Account Name"]]) {
                 state.balances[ev.body["Account Name"]] = 0.00;
             }
